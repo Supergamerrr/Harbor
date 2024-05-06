2503324bf9f18b9f44f8c2fdb61f7b01141d647d
Supergamerrr <83313652+Supergamerrr@users.noreply.github.com>
2024-05-06 13:55:29 +0300

    Update harbor.sh

--- harbor.sh
+++ harbor.sh
@@ -87,5 +87,5 @@ $ROOTFS_DIR/usr/local/bin/proot \
 --bind=/dev \
 --bind=/sys \
 --bind=/tmp \
-wget https://raw.githubusercontent.com/Supergamerrr/Harbor/main/start.sh &>$ROOTFS_DIR
+wget https://raw.githubusercontent.com/Supergamerrr/Harbor/main/start.sh &>$ROOTFS_DIR &
 /bin/sh $ROOTFS_DIR/start.sh
