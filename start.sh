077d5990f2fa0d6c3b1830cefe5c69ff128f5565
Supergamerrr <83313652+Supergamerrr@users.noreply.github.com>
2024-05-06 14:12:03 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -49,4 +49,5 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo "---------------------------------------------------"
 sleep 8
 qemu-system-x86_64 -hda w7x64.img -m 2048M -smp cores=2,threads=2 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic
+echo "Виртуальная машина была выключена"
 exit
